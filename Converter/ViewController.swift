63ec7e4cdf51a9517b3b91bf63a249c9c82a2d25
shojishunsuke <shojishunshun@gmail.com>
2019-05-02 21:30:05 +0900

    Initial Commit

---
+++ Converter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Converter
+//
+//  Created by 庄司隼介 on 2019/05/02.
+//  Copyright © 2019 庄司隼介. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ConverterTests/ConverterTests.swift
@@ -0,0 +1,34 @@
+//
+//  ConverterTests.swift
+//  ConverterTests
+//
+//  Created by 庄司隼介 on 2019/05/02.
+//  Copyright © 2019 庄司隼介. All rights reserved.
+//
+
+import XCTest
+@testable import Converter
+
+class ConverterTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
